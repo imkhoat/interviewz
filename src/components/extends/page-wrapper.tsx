e02ff4755ad51c09266450f911531777a440cd9f
imkhoat <khoatbui.idea@gmail.com>
2025-02-16 14:06:21 +0700

    feat: imporve  page wrapper hook

--- src/components/extends/page-wrapper.tsx
+++ src/components/extends/page-wrapper.tsx
@@ -1,18 +1,27 @@
+"use client"
+
+import { useContext } from 'react';
+import { usePageWrapper } from '@/hooks/use-page-wrapper';
 import PageWrapperHeader from "@/components/extends/page-wrapper-header";
+import PageWrapperSidebar from "@/components/extends/page-wrapper-sidebar";
 
 export default function PageWrapper({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const { PageWrapperStateContext, PageWrapperConfigContext } = usePageWrapper();
+  const state = useContext(PageWrapperStateContext);
+  const config = useContext(PageWrapperConfigContext);
+
   return (
     <div className="page-wrapper w-full h-full grid grid-cols-12 gap-4">
-      <div className="__sidebar col-span-3 bg-slate-50">
-
+      <div className={config?.sidebar ? " col-span-3 bg-slate-50" : "hidden"}>
+        { config?.sidebar ? <PageWrapperSidebar /> : null }
       </div>
-      <div className="__main col-span-9 flex flex-col justify-start items-stretch space-y-4">
-        <div className="__header bg-slate-50">
-          <PageWrapperHeader />
+      <div className={config?.sidebar ? " col-span-9 flex flex-col justify-start items-stretch space-y-4" : "__main col-span-12 flex flex-col justify-start items-stretch space-y-4"}>
+        <div className={config?.header ? " bg-slate-50": "hidden"}>
+          { config?.header ? <PageWrapperHeader /> : null }
         </div>
         <div className="__body bg-slate-50">
           {children}
--- src/hooks/use-page-wrapper.ts
+++ src/hooks/use-page-wrapper.ts
@@ -1,3 +1,48 @@
-function usePageWrapper() {
+"use client"
+
+import * as React from "react"
+
+interface PageWrapperState {
+
+}
+
+interface PageWrapperConfig {
+  header: boolean;
+  sidebar: boolean;
+  footer: boolean;
+  title: boolean;
+  description: boolean;
+}
+
+const PageWrapperStateContext = React.createContext<PageWrapperState | undefined>(undefined);
+const PageWrapperConfigContext = React.createContext<PageWrapperConfig>({
+  header: true,
+  sidebar: true,
+  footer: true,
+  title: true,
+  description: true,
+});
+
+export function usePageWrapper() {
+
+  function setPageWrapperState() {
+
+  }
+
+  function setupPageWrapperConfig(config: PageWrapperConfig) {
+    return {
+      header: config.header,
+      sidebar: config.sidebar,
+      footer: config.footer,
+      title: config.title,
+      description: config.description,
+    }
+
+  }
   
+  return {
+    PageWrapperStateContext,
+    PageWrapperConfigContext,
+    setPageWrapperState,
+  }
 }
