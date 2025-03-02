878d063c63bd4f9ee9e35cea9af3c4bb0a28986d
imkhoat <khoatbui.idea@gmail.com>
2025-03-02 14:43:49 +0700

    fix: change icon for AI generate button

--- src/app/(admin)/resume-builder/page.tsx
+++ src/app/(admin)/resume-builder/page.tsx
@@ -1,7 +1,18 @@
+
+'use strict';
+
+import { Image } from 'lucide-react'
+import SectionProfile from '@/app/(admin)/resume-builder/_components/section-profile'
+
 export default function ResumeBuilderPage() {
   return (
-    <div>
-      <h1>Resume Builder</h1>
+    <div className="py-8 flex flex-row justify-start items-start gap-8">
+      <div className="min-h-screen w-1/2 border border-primary-foreground rounded-md p-4 bg-white">
+        <SectionProfile />
+      </div>
+      <div className="min-h-screen w-1/2 border border-primary-foreground rounded-md p-4 bg-white flex justify-center items-center">
+        <Image size="64" className="text-secondary"/>
+      </div>
     </div>
   );
 }
