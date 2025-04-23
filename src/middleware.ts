cf22b37df571149da2211f6053e2a20d1fd740ca
Miguel <85888237+imkhoat@users.noreply.github.com>
2025-04-24 00:05:48 +0700

    feat(auth): router protected (#37)

---
+++ src/middleware.ts
@@ -0,0 +1,52 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+const publicRoutes = [
+  "/",
+  "/auth/login",
+  "/auth/signup",
+  "/auth/reset-password",
+]
+
+const authRoutes = ["/auth/login", "/auth/signup", "/auth/reset-password"]
+
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl
+  const authCookie = request.cookies.get('auth-storage')
+
+  let isAuthenticated = false
+
+  if (authCookie) {
+    try {
+      const authData = JSON.parse(authCookie.value)
+      isAuthenticated = !!authData.state?.isAuthenticated
+    } catch (error) {
+      console.error("Error parsing auth cookie:", error)
+    }
+  }
+
+  if (isAuthenticated && authRoutes.includes(pathname)) {
+    return NextResponse.redirect(new URL('/', request.url))
+  }
+
+  if (!isAuthenticated && !publicRoutes.includes(pathname)) {
+    return NextResponse.redirect(new URL('/auth/login', request.url))
+  }
+
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
+  ],
+}
