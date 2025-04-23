9ba7217684c5f88eecaea834598bb15c5ee24a19
Miguel <85888237+imkhoat@users.noreply.github.com>
2025-04-23 07:26:30 +0700

    feat(auth): implement signup with email and password (#34)

--- src/modules/auth/components/form-signup.tsx
+++ src/modules/auth/components/form-signup.tsx
@@ -1,4 +1,3 @@
-
 import {
   Form,
   FormControl,
@@ -6,19 +5,23 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@shared/components/ui/form"
-import Link from "next/link"
-import { User2Icon } from "lucide-react"
-import { Input } from "@shared/components/ui/input"
-import { Button } from "@shared/components/ui/button"
-import { useFormSignup } from "@auth/hooks/form-signup"
-import { Avatar, AvatarFallback } from "@shared/components/ui/avatar"
-import { Card, CardContent, CardHeader, CardTitle, CardDescription } from '@shared/components/ui/card'
-
+} from "@shared/components/ui/form";
+import Link from "next/link";
+import { User2Icon } from "lucide-react";
+import { Input } from "@shared/components/ui/input";
+import { Button } from "@shared/components/ui/button";
+import { useFormSignup } from "@auth/hooks/form-signup";
+import { Avatar, AvatarFallback } from "@shared/components/ui/avatar";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+} from "@shared/components/ui/card";
 
 export default function FormChangePassword() {
-
-  const { form, onSubmit } = useFormSignup()
+  const { form, onSubmit } = useFormSignup();
 
   return (
     <Card className="page-login shadow-none border-0">
@@ -37,11 +40,57 @@ export default function FormChangePassword() {
           </AvatarFallback>
         </Avatar>
         <CardTitle>Create new account</CardTitle>
-        <CardDescription>Enter your email to create new account</CardDescription>
+        <CardDescription>
+          Enter your email to create new account
+        </CardDescription>
       </CardHeader>
       <CardContent className="pb-6">
         <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="w-full flex flex-col justify-start items-stretch space-y-6">
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="w-full flex flex-col justify-start items-stretch space-y-6"
+          >
+            <div className="flex flex-row space-x-2">
+              <FormField
+                control={form.control}
+                name="firstName"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>First name</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Dean" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="lastName"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Last name</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Smith" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <FormField
+              control={form.control}
+              name="fullName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Full name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Dean Smith" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
             <FormField
               control={form.control}
               name="email"
@@ -83,13 +132,21 @@ export default function FormChangePassword() {
             />
             <div className="flex flex-col space-y-2">
               <Button type="submit">Signup with email</Button>
-              <Button type="submit" variant={'secondary'}>Signup with Google</Button>
-              <Button type="submit" variant={'secondary'}>Signup with Github</Button>
-              <Button type="button" variant={'link'}><Link href={'/auth/login'}>Found an account? Back to login</Link></Button>
+              <Button type="submit" variant={"secondary"}>
+                Signup with Google
+              </Button>
+              <Button type="submit" variant={"secondary"}>
+                Signup with Github
+              </Button>
+              <Button type="button" variant={"link"}>
+                <Link href={"/auth/login"}>
+                  Found an account? Back to login
+                </Link>
+              </Button>
             </div>
           </form>
         </Form>
       </CardContent>
     </Card>
-  )
+  );
 }
